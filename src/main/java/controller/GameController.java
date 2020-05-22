70e0000fffa7dc04c25e6b00d54eea4420ef1967
krisztian2545 <krisztian2545@gmail.com>
2020-05-22 22:44:04 +0200

    sleep time

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,4 +1,9 @@
 package controller;
 
 public class GameController {
+
+    public void initData(String username, String botname, boolean punish, boolean revard) {
+
+    }
+
 }
--- src/main/java/controller/LaunchController.java
+++ src/main/java/controller/LaunchController.java
@@ -1,4 +1,69 @@
 package controller;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LaunchController {
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private TextField player_name;
+
+    @FXML
+    private TextField new_enemy_name;
+
+    @FXML
+    private ToggleGroup enemy;
+
+    @FXML
+    private CheckBox punish;
+
+    @FXML
+    private CheckBox revard;
+
+    @FXML
+    private ChoiceBox enemy_list;
+
+
+    @FXML
+    public void initialize() {
+
+
+    }
+
+    @FXML
+    public void startGame(ActionEvent event) throws IOException {
+        if (player_name.getText().isEmpty()) {
+            errorLabel.setText("* Username is empty!");
+        }
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/game.fxml"));
+        Parent root = fxmlLoader.load();
+        if(enemy.)
+        fxmlLoader.<GameController>getController().initData(player_name.getText(), );
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+        log.info("Username is set to {}, loading game scene.", usernameTextfield.getText());
+    }
+
+    private ObservableList getEnemyNames() {
+        ObservableList out;
+        // File.list();
+
+        return out;
+    }
+
 }
